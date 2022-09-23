f7ddac569633662a3d12c9118d4eb9599f7f9825
CassieL <cassiexl59@gmail.com>
2022-09-22 22:39:04 -0700

    Separate Javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+ function bigFunction(){
+    document.getElementById("profilephoto").style.width ="14%";
+    document.getElementById("profilephoto").style.height = "14%";
+}
